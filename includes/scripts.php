4dfe96f10d5a5a1d2f7a3e8c10ba94e7cda30e61
wsuwebsupport <42049824+wsuwebsupport@users.noreply.github.com>
2024-02-01 09:36:40 -0800

    Update scripts.php
    remove class

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,16 +3,8 @@
 class Scripts {
 
 
-    public static function setup_classes() {
-
-        // Load any of the /classes/... used
-
-    }
-
-
     public static function init() {
 
-        self::setup_classes();
 
         // Do plugin stuff here
 
